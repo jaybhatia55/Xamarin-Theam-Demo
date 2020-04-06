fc92b3edf199dd599593c4063ff92db373208332
JAY BHATIA <jaybhatia55@yahoo.com>
2020-04-06 14:38:43 +0530

    Theam Chnage based on light and dark

---
+++ TheamDemo/TheamDemo/TheamDemo.iOS/Renders/PageRender.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Xamarin.Forms;
+using TheamDemo.iOS.Renders;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+
+[assembly :ExportRenderer (typeof(ContentPage),typeof(PageRender))]
+namespace TheamDemo.iOS.Renders
+{
+    public class PageRender : Xamarin.Forms.Platform.iOS.PageRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement != null || Element == null)
+                return;
+
+            try
+            {
+                SetTheme();
+            }
+            catch (Exception ex)
+            {
+
+            }
+                    
+        }
+        public override void TraitCollectionDidChange(UITraitCollection previousTraitCollection)
+        {
+            base.TraitCollectionDidChange(previousTraitCollection);
+            if (TraitCollection.UserInterfaceStyle != previousTraitCollection.UserInterfaceStyle)
+                SetTheme();
+
+        }
+        private void SetTheme()
+        {
+            App.ApplyTheme();
+
+        }
+    }
+}
+
---
+++ TheamDemo/TheamDemo/TheamDemo/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TheamDemo.Theam;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TheamDemo
+{
+    public partial class App : Application
+    {
+        public static void ApplyTheme()
+        {
+            if (AppInfo.RequestedTheme == AppTheme.Dark)
+            {
+                // Change to dark theme
+               App.Current.Resources = new DarkTheme();
+            }
+            else
+            {
+                // Change to light theme
+                App.Current.Resources = new LightTheme();
+            }
+        }
+        public App()
+        {
+            ApplyTheme();
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
